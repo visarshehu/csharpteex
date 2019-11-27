550f08ec8848ec2971d3504daf1e62a765681b53
Ron Demiri <rd27240@seeu.edu.mk>
2019-11-27 09:08:19 +0100

    code

---
+++ WebApplication1/WebApplication1/Models/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<TestModel> TestModels { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlServer("Server=rd27240EX01;Database=DB02;Trusted_Connection=True;");
+        }
+             
+    }
+}
